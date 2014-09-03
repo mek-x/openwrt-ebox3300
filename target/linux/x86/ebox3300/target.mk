b0ac3bcded8c266b593716012f7c2e3d4b2d8976
mek_x <mek.xgt@gmail.com>
2014-09-03 19:28:19 +0200

    ebox3300: added ebox3300 target

---
+++ target/linux/x86/ebox3300/target.mk
@@ -0,0 +1,10 @@
+BOARDNAME:=EBOX-3300A
+FEATURES:=ext4 pci usb
+DEFAULT_PACKAGES += kmod-ata-core kmod-ata-pata-821x \
+                    kmod-r6040 \
+                    kmod-usb-core kmod-usb-ohci kmod-usb2
+
+define Target/Description
+	Build firmware images for DMP Electronics EBOX3300 mini pc
+endef
+
