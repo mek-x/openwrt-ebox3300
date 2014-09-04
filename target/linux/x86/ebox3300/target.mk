723702f6baed6f17680a90ed4237ed926e6e5289
mek_x <mek.xgt@gmail.com>
2014-09-04 20:00:01 +0200

    ebox3300: modified to use ext4 out of the box

--- target/linux/x86/ebox3300/target.mk
+++ target/linux/x86/ebox3300/target.mk
@@ -1,6 +1,6 @@
 BOARDNAME:=EBOX-3300A
 FEATURES:=ext4 pci usb
-DEFAULT_PACKAGES += kmod-ata-core kmod-ata-pata-821x \
+DEFAULT_PACKAGES += kmod-ata-core \
                     kmod-r6040 \
                     kmod-usb-core kmod-usb-ohci kmod-usb2
 
